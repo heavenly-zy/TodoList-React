83720670c1d0e464a7fc86c34e73030c81f210ca
zy <1457715339@qq.com>
2020-03-19 17:30:37 +0800

    把 UI 组件改为无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,33 +1,31 @@
 import React, { Component, Fragment } from 'react'
 import { Input, Button, List } from 'antd';
 
-class TodoListUI extends Component {
-  render() {
-    return (
-      <Fragment>
-        <div className="wrapper">
-          <div>
-            <Input
-              placeholder="输入内容"
-              id="insetContent"
-              value={this.props.inputValue}
-              onChange={this.props.handleInputChange}
-            />
-            <Button type="primary" onClick={this.props.handleBtnClick}>提交</Button>
-          </div>
-          <List
-            bordered
-            dataSource={this.props.getTodoItem()}
-            renderItem={item => (
-              <List.Item>
-                {item}
-              </List.Item>
-            )}
+const TodoListUI = (props) => {
+  return (
+    <Fragment>
+      <div className="wrapper">
+        <div>
+          <Input
+            placeholder="输入内容"
+            id="insetContent"
+            value={props.inputValue}
+            onChange={props.handleInputChange}
           />
+          <Button type="primary" onClick={props.handleBtnClick}>提交</Button>
         </div>
-      </Fragment>
-    )
-  }
+        <List
+          bordered
+          dataSource={props.getTodoItem()}
+          renderItem={item => (
+            <List.Item>
+              {item}
+            </List.Item>
+          )}
+        />
+      </div>
+    </Fragment>
+  )
 }
 
 export default TodoListUI;
